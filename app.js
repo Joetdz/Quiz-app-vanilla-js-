8d221428bb5162edc5fd1a0edce822d68b2e55b7
fdis111 <disubif@gmail.com>
2022-06-01 09:10:51 +0100

    refactor: home page form submit event handler

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const btnStart = document.querySelector(".btn-start");
 const btnNext = document.querySelector(".btn-next");
 const btnQuit = document.querySelector(".btn-quit");
 const btnHome = document.querySelector(".btn-home");
-//TODO retrieve dynamically input fields
-const playerName = document.querySelector(".player-name");
-const playerMail = document.querySelector(".player-mail");
+//TODO
+const playerNameContainer = document.querySelector(".player-name");
+const playerMailContainer = document.querySelector(".player-mail");
 
 const startContainer = document.querySelector(".container-start");
 const questionContainer = document.querySelector(".container-questions");
@@ -18,8 +18,6 @@ let choixReponseSelect = document.querySelector("input[type=radio]");
 const spanScore = document.querySelector(".score");
 const quizCounter = document.querySelector(".quiz-count");
 let progressBar = document.querySelector(".rebours");
-let errorsnom = 0;
-let errorsmail = 0;
 let nomError = document.querySelector(".error-name");
 let mailError = document.querySelector(".error-mail");
 let bordureErrorNom = document.querySelector(".name");
@@ -29,154 +27,68 @@ let answerTrue = 0;
 let answerSelected = 0;
 let count = 0;
 let rebours = 60;
+let playerScore = 0;
 
-// on soumet le formulaire et
-form.addEventListener("submit", function (e) {
+function validate(name, email) {
+  let nameErrors;
+  let emailErrors;
+
+  if (name === "") {
+    nameErrors = true;
+    bordureErrorNom.style.border = "solid 1px red";
+    nomError.textContent =
+      "N’oubliez pas de renseigner votre nom avant de commencer le Quiz";
+  } else {
+    nomError.textContent = " ";
+    nameErrors = false;
+    bordureErrorNom.style.border = "solid 2px rgb(233, 233, 233)";
+  }
+  let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (email.match(mailformat)) {
+    mailError.textContent = " ";
+    emailErrors = false;
+    bordureErrormail.style.border = "solid 2px rgb(233, 233, 233)";
+  } else {
+    emailErrors = true;
+    bordureErrormail.style.border = "solid 1px red";
+    mailError.textContent =
+      "Vueillez  renseigner un  email valide avant de commencer le Quiz";
+  }
+  return { nameErrors, emailErrors };
+}
+
+function showElement(element) {
+  element.style.display = "grid";
+  element.style.opacity = 1;
+}
+function hideElement(element) {
+  element.style.display = "none";
+}
+
+function handleFormSubmit(e) {
   e.preventDefault();
-  const nom = form.querySelector("#name").value;
-  const mail = document.querySelector("#mail").value;
-  //on appel la fonction de validation
-  validation(nom, mail);
-  //s'il n'ya pas d'erreur on affiche le conteneur des question
-  if (errorsnom == false && errorsmail == false) {
-    affichage_Container(startContainer, questionContainer);
-    playerName.textContent = nom;
-    playerMail.textContent = mail;
 
-    // on recharge la page si on clique le le btn quit
-    btnQuit.addEventListener("click", function (e) {
-      e.preventDefault();
-      affichage_Container(questionContainer, resultContainer);
+  const playerName = form.querySelector("#name").value;
+  const playerEmail = form.querySelector("#mail").value;
 
-      // verifie si le score depasse la moitié de la ail du tableau des questions
-      if (playerScore >= questions.length / 2) {
-        iconesResultat.classList.remove("fa-circle-xmark");
-        iconesResultat.classList.add("fa-circle-check");
-        console.log(iconesResultat);
-      }
-      spanScore.textContent = playerScore + "/" + questions.length;
+  const { nameErrors, emailErrors } = validate(playerName, playerEmail);
 
-      btnHome.addEventListener("click", function (e) {
-        e.preventDefault();
-        document.location.reload(true);
-      });
-    });
-
-    function timer() {
-      if (rebours > 0) {
-        rebours -= 1;
-        document.querySelector(".count-time").textContent = rebours;
-      } else if (rebours == 0) {
-        if (document.querySelector("input[type=radio]:checked")) {
-          answerSelected = document.querySelector(
-            "input[type=radio]:checked"
-          ).value;
-          answerTrue = questions[count].reponse;
-          if (answerSelected == answerTrue) {
-            playerScore += 1;
-          }
-          document.querySelector(
-            "input[type=radio]:checked"
-          ).parentNode.style.border = "";
-        } else {
-        }
-        count += 1;
-        progressBar.remove();
-        rebours = 60;
-
-        progressBar = document.createElement("div");
-        progressBar.classList.add("rebours");
-        document.querySelector(".timer").appendChild(progressBar);
-
-        progressBar.classList.add("rebours");
-        rebours -= 1;
-        document.querySelector(".count-time").textContent = rebours;
-
-        if (count < questions.length) {
-          quizCounter.textContent = count + 1 + "/" + questions.length;
-          questionLabel.textContent = questions[count].question;
-          let choix = questions[count].choix;
-          //on appel la fonction qui affiche les assertions de la question
-          affichage_assertions(choix);
-        } else {
-          affichage_Container(questionContainer, resultContainer);
-
-          // verifie si le score depasse la moitié de la ail du tableau des questions
-          if (playerScore >= questions.length / 2) {
-            iconesResultat.classList.remove("fa-circle-xmark");
-            iconesResultat.classList.add("fa-circle-check");
-          }
-          spanScore.textContent = playerScore + "/" + questions.length;
-          btnHome.addEventListener("click", function (e) {
-            e.preventDefault();
-            document.location.reload(true);
-          });
-        }
-      }
-      return rebours;
-    }
-    let compteur = setInterval(timer, 1000);
-
-    // affiche la premiere question
+  if (nameErrors == false && emailErrors == false) {
+    hideElement(startContainer);
+    showElement(questionContainer);
+    playerNameContainer.textContent = playerName;
+    playerMailContainer.textContent = playerEmail;
 
     quizCounter.textContent = count + 1 + "/" + questions.length;
     questionLabel.textContent = questions[count].question;
     let choix = questions[count].choix;
 
     affichage_assertions(choix);
-    let playerScore = 0;
-    // btn suivant
-    btnNext.addEventListener("click", function (e) {
-      e.preventDefault();
-
-      //on verifie la reponse choix et on update le score
-      if (document.querySelector("input[type=radio]:checked")) {
-        rebours = 60;
-        progressBar.remove();
-        progressBar = document.createElement("div");
-        progressBar.classList.add("rebours");
-        document.querySelector(".timer").appendChild(progressBar);
-        console.log(progressBar);
-        progressBar.classList.add("rebours");
-        answerSelected = document.querySelector(
-          "input[type=radio]:checked"
-        ).value;
-        count += 1;
-        answerTrue = questions[count - 1].reponse;
-        if (answerSelected == answerTrue) {
-          playerScore += 1;
-        }
-
-        if (count < questions.length) {
-          quizCounter.textContent = count + 1 + "/" + questions.length;
-          choix = questions[count].choix;
-          questionLabel.textContent = questions[count].question;
-          affichage_assertions(choix);
-        } else {
-          affichage_Container(questionContainer, resultContainer);
-
-          // verifie si le score depasse la moitié de la ail du tableau des questions
-          if (playerScore >= questions.length / 2) {
-            iconesResultat.classList.remove("fa-circle-xmark");
-            iconesResultat.classList.add("fa-circle-check");
-            console.log(iconesResultat);
-          }
-          spanScore.textContent = playerScore + "/" + questions.length;
-
-          btnHome.addEventListener("click", function (e) {
-            e.preventDefault();
-            document.location.reload(true);
-          });
-        }
-      }
-    });
   }
   return;
-});
+}
 
-//MES FONCTIONS
-
-//quand on clique sur une assertion, on change la couleur de la bordure
+form.addEventListener("submit", handleFormSubmit);
 
 function Choix_select(e) {
   border_reinit(e.parentNode["reponse"]);
@@ -199,7 +111,6 @@ function Choix_select(e) {
   }
 }
 
-// reintitialisation de la couleur la  bordue d'une assertion par defaut
 function border_reinit(param) {
   param.forEach((element) => {
     element.parentNode.style.border = "";
@@ -207,30 +118,6 @@ function border_reinit(param) {
 }
 
 // on cree une fonction pour la validation des mail et du nom
-function validation(name, email) {
-  if (name === "") {
-    errorsnom = true;
-    bordureErrorNom.style.border = "solid 1px red";
-    nomError.textContent =
-      "N’oubliez pas de renseigner votre nom avant de commencer le Quiz";
-  } else {
-    nomError.textContent = " ";
-    errorsnom = false;
-    bordureErrorNom.style.border = "solid 2px rgb(233, 233, 233)";
-  }
-  let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (email.match(mailformat)) {
-    mailError.textContent = " ";
-    errorsmail = false;
-    bordureErrormail.style.border = "solid 2px rgb(233, 233, 233)";
-  } else {
-    errorsmail = true;
-    bordureErrormail.style.border = "solid 1px red";
-    mailError.textContent =
-      "Vueillez  renseigner un  email valide avant de commencer le Quiz";
-  }
-  return;
-}
 
 // affichage des quatres  assertion disponible pour chaque question
 
@@ -250,9 +137,122 @@ function affichage_assertions(choix) {
 }
 // Afficher et cacher les conteneurs
 
-function affichage_Container(cache, affiche) {
-  cache.style.display = "none";
-  cache.style.opacity = "0";
-  affiche.style.display = "grid";
-  affiche.style.opacity = "1";
+btnQuit.addEventListener("click", function (e) {
+  e.preventDefault();
+  hideElement(questionContainer);
+  showElement(resultContainer);
+
+  // verifie si le score depasse la moitié de la ail du tableau des questions
+  if (playerScore >= questions.length / 2) {
+    iconesResultat.classList.remove("fa-circle-xmark");
+    iconesResultat.classList.add("fa-circle-check");
+    console.log(iconesResultat);
+  }
+  spanScore.textContent = playerScore + "/" + questions.length;
+
+  btnHome.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.location.reload(true);
+  });
+});
+
+btnNext.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  //on verifie la reponse choix et on update le score
+  if (document.querySelector("input[type=radio]:checked")) {
+    rebours = 60;
+    progressBar.remove();
+    progressBar = document.createElement("div");
+    progressBar.classList.add("rebours");
+    document.querySelector(".timer").appendChild(progressBar);
+    console.log(progressBar);
+    progressBar.classList.add("rebours");
+    answerSelected = document.querySelector("input[type=radio]:checked").value;
+    count += 1;
+    answerTrue = questions[count - 1].reponse;
+    if (answerSelected == answerTrue) {
+      playerScore += 1;
+    }
+
+    if (count < questions.length) {
+      quizCounter.textContent = count + 1 + "/" + questions.length;
+      choix = questions[count].choix;
+      questionLabel.textContent = questions[count].question;
+      affichage_assertions(choix);
+    } else {
+      hideElement(questionContainer);
+      showElement(resultContainer);
+
+      // verifie si le score depasse la moitié de la ail du tableau des questions
+      if (playerScore >= questions.length / 2) {
+        iconesResultat.classList.remove("fa-circle-xmark");
+        iconesResultat.classList.add("fa-circle-check");
+        console.log(iconesResultat);
+      }
+      spanScore.textContent = playerScore + "/" + questions.length;
+
+      btnHome.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.location.reload(true);
+      });
+    }
+  }
+});
+
+function timer() {
+  if (rebours > 0) {
+    rebours -= 1;
+    document.querySelector(".count-time").textContent = rebours;
+  } else if (rebours == 0) {
+    if (document.querySelector("input[type=radio]:checked")) {
+      answerSelected = document.querySelector(
+        "input[type=radio]:checked"
+      ).value;
+      answerTrue = questions[count].reponse;
+      if (answerSelected == answerTrue) {
+        playerScore += 1;
+      }
+      document.querySelector(
+        "input[type=radio]:checked"
+      ).parentNode.style.border = "";
+    } else {
+    }
+    count += 1;
+    progressBar.remove();
+    rebours = 60;
+
+    progressBar = document.createElement("div");
+    progressBar.classList.add("rebours");
+    document.querySelector(".timer").appendChild(progressBar);
+
+    progressBar.classList.add("rebours");
+    rebours -= 1;
+    document.querySelector(".count-time").textContent = rebours;
+
+    if (count < questions.length) {
+      quizCounter.textContent = count + 1 + "/" + questions.length;
+      questionLabel.textContent = questions[count].question;
+      let choix = questions[count].choix;
+      //on appel la fonction qui affiche les assertions de la question
+      affichage_assertions(choix);
+    } else {
+      hideElement(questionContainer);
+      showElement(resultContainer);
+
+      // verifie si le score depasse la moitié de la ail du tableau des questions
+      if (playerScore >= questions.length / 2) {
+        iconesResultat.classList.remove("fa-circle-xmark");
+        iconesResultat.classList.add("fa-circle-check");
+      }
+      spanScore.textContent = playerScore + "/" + questions.length;
+      btnHome.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.location.reload(true);
+      });
+    }
+  }
+  return rebours;
 }
+
+let compteur = setInterval(timer, 1000);
